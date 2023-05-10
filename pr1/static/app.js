230f645247f644a27f09503e05731a1db86108f3
Pratyut <pratyut987@gmail.com>
2023-05-11 05:09:43 +0530

    init

--- pr1/static/app.js
+++ pr1/static/app.js
@@ -4,18 +4,38 @@ const chat = document.querySelector('.chat-form')
 const chatInput = document.querySelector('.chat-input')
 chat.addEventListener('submit', e => {
   e.preventDefault()
+  if((chatInput.value).trim() === ''){
+    return;
+  }
   socket.emit('chat', chatInput.value)
   chatInput.value = ''
 })
-const chatDump = document.querySelector('.chat-dump')
+const client_dump = document.querySelector('.client_dump')
+const user_dump = document.querySelector('.user_dump')
 const render = ({message, id}) => {
   const div = document.createElement('div')
-  div.classList.add('chat-message')
+  const div2 = document.createElement('div')
+  const txt = document.createElement('h5')
+  div2.classList.add('free')
+  txt.innerHTML = message
   if (id === socket.id) {
-    div.classList.add('chat-message--user')
+    div.classList.add('chat-client_div')
+    txt.classList.add('chat-message--user')
+    div.appendChild(txt)
+    user_dump.appendChild(div)
+    console.log(div.offsetHeight);
+    div2.style.height=div.offsetHeight+"px";
+    client_dump.appendChild(div2)
   }
-  div.innerText = message
-  chatDump.appendChild(div)
+  else{
+    div.classList.add('chat-user_div')
+    txt.classList.add('chat-message--client')
+    div.appendChild(txt)
+    client_dump.appendChild(div)
+    console.log(div.offsetHeight);
+    div2.style.height=div.offsetHeight+"px";
+    user_dump.appendChild(div2)
+  }
 }
 socket.on('chat', data => {
   render(data)
